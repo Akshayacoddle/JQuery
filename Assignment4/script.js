9b6f592395af33c675c9f7d6af84a1cc23f9dc95
Akshaya.kcoddle <akshaya.kcoddletech.com>
2023-09-11 12:56:47 +0530

    Assignment-4 change search input

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -3,15 +3,14 @@ $(document).ready(function () {
     let total = 0;
     let search = ''
     const arr = ['beefbiriyani', 'beefshavarma', 'biriyani', 'Choclatecake', 'dosa', 'hyderabadbiriyani', 'icecream', 'juice', 'pizza', 'shavarma']
-    $('#search').on('keydown', function (event) {
+    $('#search').on('input', function (event) {
         search += event.key;
+        let searchval = $('#search').val().toLowerCase();
         if (!(search === '')) {
-            let searchval = $('#search').val();
             arr.forEach(element => {
                 if (element.toLowerCase().includes(searchval) === false) {
                     $('#' + element).parent().hide();
                 } else {
-                    // console.log($('#search').val())
                     $('#' + element).parent().show();
                 }
             });
@@ -22,7 +21,6 @@ $(document).ready(function () {
         $('aside').toggle();
     })
     $('.btn').one('click', function (event) {
-        let cartArray = [];
         let current = $(event.target.parentElement);
         id += 1;
         element = current.find('h2').text()
@@ -31,20 +29,13 @@ $(document).ready(function () {
         $('#cartitem' + id).append('<div class="quality"><button class="minus" id="min' + id + '" >-</button><input id="val' + id + '" class="value" type="number" value= 1><button class="pluse" id="plu' + id + '">+</button></div>');
         $('#cartitem' + id).append('<div>' + current.find('.price').text() + '</div>');
         $('#cartitem' + id).append('</div>');
-        let count = 1;
         let priceid = Number(($(this).siblings('.price')).text().slice(1))
-
         total += priceid
         $('.amount').html(total)
         $(`#plu${id}`).on('click', function (e) {
             let plusid = ($(this).siblings('input')).attr('id')
-            console.log(($(this).siblings('input')).attr('id'));
-            let price = event.target.parentElement.parentElement
             let count = Number(($(this).siblings('input')).val())
             count += 1
-            let num = ($(price).text().slice(-3))
-            let totalprice = Number(num.trim()) * count
-            let cartid = $(price).attr('id')
             total += priceid
             $('#' + plusid).val(count);
             $('.amount').html(total)
@@ -54,7 +45,6 @@ $(document).ready(function () {
             console.log($(this).siblings('input'));
             if (countval > 1) {
                 let valueid = ($(this).siblings()).attr('id')
-                let count = ($(this).siblings()).val()
                 countval -= 1
                 $('#' + valueid).val(countval);
                 total -= priceid
@@ -62,14 +52,10 @@ $(document).ready(function () {
 
             } else {
                 let price = event.target.parentElement.parentElement
-                let cartid = $(price).attr('id')
                 let valueid = ($(this).siblings()).attr('id')
-                console.log(($("#" + cartid + " div:last-child").text()).slice(2));
-
                 $('#' + valueid).val(countval);
                 total -= priceid
                 $('.amount').html(total)
-                console.log(e.target.parentElement.parentElement);
                 $(e.target.parentElement.parentElement).hide();
             }
 
